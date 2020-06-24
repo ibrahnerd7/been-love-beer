f115b58a3d8e7dd50d562ef5b8d715667daaf7f6
ibrahnerd7 <ibrahmaina7@gmail.com>
2020-06-24 20:47:02 +0300

    Allow like for only authorized users

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -5,28 +5,52 @@ import Columns from 'react-bulma-components/lib/components/columns';
 import BeerCard from '../containers/BeerCard';
 import Searchbar from "./Searchbar";
 import {getBeers} from "../api/punk";
+import {Link} from "react-router-dom";
+import Media from "react-bulma-components/lib/components/media";
+import Card from "react-bulma-components/lib/components/card";
 
 const BeerList = () => {
     const [beers, setBeers] = useState([]);
     const [searchTerm, setSearchTerm] = useState("punk");
+    let [isLoggedIn, setIsLoggedIn] = useState(false)
 
     useEffect(() => {
         const fetchBeer = async () => {
-            console.log(searchTerm)
             const response = await getBeers(searchTerm)
             setBeers(response);
             return response;
         };
-        fetchBeer().then(r => console.log(r));
+        fetchBeer().then(r => console.log(1));
     }, [searchTerm]);
+
+    const setLoggedIn = (loggedIn) => {
+        setIsLoggedIn(loggedIn)
+    }
 
     return (
         <Section>
             <Container>
+                {isLoggedIn && <Columns size={1}>
+                    <Card>
+                        <Card.Content style={{margin: 4}}>
+                            <Media>
+                                <Media.Item>
+
+                                    {<Link to="/login" style={{float: "right"}}>
+                                        <u>Login</u> to save this beer
+                                    </Link>}
+                                </Media.Item>
+                            </Media>
+                        </Card.Content>
+                    </Card>
+                </Columns>}
                 <Searchbar submitSearch={(text) => setSearchTerm(text)}/>
                 <Columns>
-                    {beers && beers.map((beer) => <BeerCard beer={beer} key={beer.id}/>)}
+                    {beers && beers.map((beer) =>
+                        <BeerCard beer={beer} key={beer.id}
+                                  setLoggedIn={(loggedIn) => setLoggedIn(loggedIn)}/>)}
                 </Columns>
+
             </Container>
         </Section>
     );
--- src/containers/BeerCard.js
+++ src/containers/BeerCard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import Card from 'react-bulma-components/lib/components/card';
 import Media from 'react-bulma-components/lib/components/media';
 import Image from 'react-bulma-components/lib/components/image';
@@ -7,57 +7,72 @@ import Heading from 'react-bulma-components/lib/components/heading';
 import Columns from 'react-bulma-components/lib/components/columns';
 import IosHeartOutline from 'react-ionicons/lib/IosHeartOutline'
 import IosHeart from 'react-ionicons/lib/IosHeart'
+import {UserContext} from "../providers/UserProvider";
 
 
-const BeerCard = ({ beer }) => {
-  let [isFavorite, setIsFavorite] = useState(false)
-  return (
-      <Columns.Column size={3} style={{margin: 16}}>
-        <Card>
-          <Card.Content style={{margin: 4}}>
-            <Media>
-              <Media.Item renderAs="figure" position="left">
-                <Image size={32} alt="64x64" src={beer.image_url}/>
-              </Media.Item>
-              <Media.Item>
-                <Heading
-                    size={4}
-                    style={{
-                      overflow: 'hidden',
-                      whiteSpace: 'normal',
-                      height: '1.2em',
-                    }}
-                >
-                  {isFavorite ?
-                      < IosHeart style={{float: "right"}} onClick={()=>setIsFavorite(!isFavorite)}/>
-                      :<IosHeartOutline
-                          style={{float: "right"}}
-                          onClick={()=>setIsFavorite(!isFavorite)}
-                      />
-                  }
-                  {beer.name}
-                </Heading>
+const BeerCard = ({beer,setLoggedIn}) => {
+    let [isFavorite, setIsFavorite] = useState(false)
+    let user = useContext(UserContext)
 
-                <div/>
-                <p style={{
-                  marginStart: 16,
-                  marginTop: 0,
-                  overflow: 'hidden',
-                  whiteSpace: 'normal',
-                  height: '4.6em',
-                  textOverflow: '-o-ellipsis-lastline',
-                }}
-                >
-                  {beer.description}
-                </p>
-              </Media.Item>
-            </Media>
 
-            <Content/>
-          </Card.Content>
-        </Card>
-      </Columns.Column>
-  )
+    let handleFavouriteClick = () => {
+        if (user) {
+            setIsFavorite(!isFavorite)
+        } else {
+            setLoggedIn(true)
+        }
+    };
+    return (
+        <Columns.Column size={3} style={{margin: 16}}>
+            <Card>
+                <Card.Content style={{margin: 4}}>
+                    <Media>
+                        <Media.Item renderAs="figure" position="left">
+                            <Image size={32} alt="64x64" src={beer.image_url}/>
+                        </Media.Item>
+                        <Media.Item>
+                            <Heading
+                                size={4}
+                                style={{
+                                    overflow: 'hidden',
+                                    whiteSpace: 'normal',
+                                    height: '1.2em',
+                                }}
+                            >
+                                {isFavorite ?
+                                    < IosHeart
+                                        style={{float: "right"}}
+                                        color="red"
+                                        onClick={() => setIsFavorite(!isFavorite)}
+                                    />
+                                    : <IosHeartOutline
+                                        style={{float: "right"}}
+                                        onClick={handleFavouriteClick}
+                                    />
+                                }
+                                {beer.name}
+                            </Heading>
+
+                            <div/>
+                            <p style={{
+                                marginStart: 16,
+                                marginTop: 0,
+                                overflow: 'hidden',
+                                whiteSpace: 'normal',
+                                height: '4.6em',
+                                textOverflow: '-o-ellipsis-lastline',
+                            }}
+                            >
+                                {beer.description}
+                            </p>
+                        </Media.Item>
+                    </Media>
+
+                    <Content/>
+                </Card.Content>
+            </Card>
+        </Columns.Column>
+    )
 }
 
 export default BeerCard;
