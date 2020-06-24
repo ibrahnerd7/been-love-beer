c98949a99d41f5e687083b2dcb10556c6a5efa2c
ibrahnerd7 <ibrahmaina7@gmail.com>
2020-06-24 19:31:47 +0300

    Add like icon and toggle

--- src/containers/BeerCard.js
+++ src/containers/BeerCard.js
@@ -1,56 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from 'react-bulma-components/lib/components/card';
 import Media from 'react-bulma-components/lib/components/media';
 import Image from 'react-bulma-components/lib/components/image';
 import Content from 'react-bulma-components/lib/components/content';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Columns from 'react-bulma-components/lib/components/columns';
-import Icon from "react-bulma-components/lib/components/icon";
+import IosHeartOutline from 'react-ionicons/lib/IosHeartOutline'
+import IosHeart from 'react-ionicons/lib/IosHeart'
 
-const BeerCard = ({ beer }) => (
 
-  <Columns.Column size={3} style={{ margin: 16 }}>
-    <Card>
-      <Card.Content style={{ margin: 4 }}>
-        <Media>
-          <Media.Item renderAs="figure" position="left">
-            <Image size={32} alt="64x64" src={beer.image_url} />
-          </Media.Item>
-          <Media.Item>
-            <Heading
-              size={4}
-              style={{
-                overflow: 'hidden',
-                whiteSpace: 'normal',
-                height: '1.2em',
-              }}
-            >
-              {beer.name}
-            </Heading>
-            <div />
-            <p style={{
-              marginStart: 16,
-              marginTop: 0,
-              overflow: 'hidden',
-              whiteSpace: 'normal',
-              height: '4.6em',
-              textOverflow: '-o-ellipsis-lastline',
-            }}
-            >
-              {beer.description}
-            </p>
-          </Media.Item>
-        </Media>
+const BeerCard = ({ beer }) => {
+  let [isFavorite, setIsFavorite] = useState(false)
+  return (
+      <Columns.Column size={3} style={{margin: 16}}>
+        <Card>
+          <Card.Content style={{margin: 4}}>
+            <Media>
+              <Media.Item renderAs="figure" position="left">
+                <Image size={32} alt="64x64" src={beer.image_url}/>
+              </Media.Item>
+              <Media.Item>
+                <Heading
+                    size={4}
+                    style={{
+                      overflow: 'hidden',
+                      whiteSpace: 'normal',
+                      height: '1.2em',
+                    }}
+                >
+                  {isFavorite ?
+                      < IosHeart style={{float: "right"}} onClick={()=>setIsFavorite(!isFavorite)}/>
+                      :<IosHeartOutline
+                          style={{float: "right"}}
+                          onClick={()=>setIsFavorite(!isFavorite)}
+                      />
+                  }
+                  {beer.name}
+                </Heading>
 
-        <Icon>
-          <span className="fas fa-check-square" />
-        </Icon>
+                <div/>
+                <p style={{
+                  marginStart: 16,
+                  marginTop: 0,
+                  overflow: 'hidden',
+                  whiteSpace: 'normal',
+                  height: '4.6em',
+                  textOverflow: '-o-ellipsis-lastline',
+                }}
+                >
+                  {beer.description}
+                </p>
+              </Media.Item>
+            </Media>
 
-        <Content />
-      </Card.Content>
-    </Card>
-  </Columns.Column>
-
-);
+            <Content/>
+          </Card.Content>
+        </Card>
+      </Columns.Column>
+  )
+}
 
 export default BeerCard;
